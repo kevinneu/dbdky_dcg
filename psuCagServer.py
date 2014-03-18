d46cf92cf8b3344c4ffeb3f4457ed7b5d99e312e
kevin <693512779@qq.com>
2014-03-18 10:04:24 +0800

    cp

--- psuCagServer.py
+++ psuCagServer.py
@@ -8,19 +8,31 @@ from psuCagHandler import *
 class psuCagServer(asyncore.dispatcher):
 	def __init__(self, address):
 		asyncore.dispatcher.__init__(self)
-		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.bind(address)
-		self.address = self.socket.getsockname()
-		self.listen(1)
+		self.logger =  logging.getLogger('psuCagServer')
+		self.addr = address
+		# self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		# self.bind(address)
+		# self.address = self.socket.getsockname()
+		# self.listen(5)
+		self._init_listen()
 
 		return
 
 	def handle_accept(self):
+		self.logger.debug('handle_accept')
 		client_info = self.accept()
 		psuCagHandler(sock=client_info[0])
-		self.handle_close()
+		#self.handle_close()
 
 		return
 
 	def handle_close(self):
-		self.close()
+		self.logger.debug('handle_close')
+		self._init_listen()
+		#self.close()
+
+	def _init_listen(self):
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.bind(self.addr)
+		self.address = self.socket.getsockname()
+		self.listen(1)	
